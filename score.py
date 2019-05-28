fix on eer computation

score.py
@@ -11,13 +11,12 @@ import scipy.io as sio
 from sklearn import metrics
 
 def compute_eer(y, y_score):
-
-	pred = [0 if x=='spoof' else 1 for x in y]
-
-	fpr, tpr, thresholds = metrics.roc_curve(pred, y_score, pos_label=1)
+	fpr, tpr, thresholds = metrics.roc_curve(y, y_score, pos_label=1)
 	fnr = 1 - tpr
-	eer_threshold = thresholds[np.nanargmin(np.abs(fnr-fpr))]
-	eer = fpr[np.nanargmin(np.abs(fnr-fpr))]
+
+	t = np.nanargmin(np.abs(fnr-fpr))
+	eer_low, eer_high = min(fnr[t],fpr[t]), max(fnr[t],fpr[t])
+	eer = (eer_low+eer_high)*0.5
 
 	return eer
 

train_loop.py
@@ -11,11 +11,12 @@ from tqdm import tqdm
 from sklearn import metrics
 
 def compute_eer(y, y_score):
-
 	fpr, tpr, thresholds = metrics.roc_curve(y, y_score, pos_label=1)
 	fnr = 1 - tpr
-	eer_threshold = thresholds[np.nanargmin(np.abs(fnr-fpr))]
-	eer = fpr[np.nanargmin(np.abs(fnr-fpr))]
+
+	t = np.nanargmin(np.abs(fnr-fpr))
+	eer_low, eer_high = min(fnr[t],fpr[t]), max(fnr[t],fpr[t])
+	eer = (eer_low+eer_high)*0.5
 
 	return eer
 