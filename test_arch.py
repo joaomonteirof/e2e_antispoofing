000f440fac53fdbf5b5141c5af580e988af1a384
joaomonteirof <joaomonteirof@gmail.com>
2019-07-02 22:42:06 -0400

    smaller resnets

--- test_arch.py
+++ test_arch.py
@@ -10,7 +10,7 @@ from data_load import Loader
 
 # Training settings
 parser = argparse.ArgumentParser(description='Test new architectures')
-parser.add_argument('--model', choices=['lstm', 'resnet', 'resnet_pca', 'lcnn_9', 'lcnn_29', 'lcnn_9_pca', 'lcnn_29_pca', 'lcnn_9_prodspec', 'lcnn_9_icqspec', 'lcnn_9_CC', 'lcnn_29_CC', 'resnet_34_CC', 'all'], default='resnet', help='Model arch')
+parser.add_argument('--model', choices=['lstm', 'resnet', 'resnet_pca', 'lcnn_9', 'lcnn_29', 'lcnn_9_pca', 'lcnn_29_pca', 'lcnn_9_prodspec', 'lcnn_9_icqspec', 'lcnn_9_CC', 'lcnn_29_CC', 'resnet_CC', 'all'], default='resnet', help='Model arch')
 args = parser.parse_args()
 
 if args.model == 'lstm' or args.model == 'all':
@@ -68,8 +68,8 @@ if args.model == 'lcnn_29_CC' or args.model == 'all':
 	model = model_.lcnn_29layers_CC()
 	mu = model.forward(batch)
 	print('lcnn_29_CC', mu.size())
-if args.model == 'resnet_34_CC' or args.model == 'all':
+if args.model == 'resnet_CC' or args.model == 'all':
 	batch = torch.rand(3, 1, 90, 300)
-	model = model_.ResNet_34_CC()
+	model = model_.ResNet_CC()
 	mu = model.forward(batch)
-	print('resnet_34_CC', mu.size())
+	print('resnet_CC', mu.size())
