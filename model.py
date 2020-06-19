f44b0682657087a485bc5605b4981eb38648083d
joaomonteirof <joaomonteirof@gmail.com>
2020-06-19 14:09:44 -0400

    self attentive aggregation over depth

--- model.py
+++ model.py
@@ -1016,7 +1016,7 @@ class TDNN(nn.Module):
 
 class TDNN_multipool(nn.Module):
 
-	def __init__(self, nclasses=-1, ncoef=90, init_coef=0):
+	def __init__(self, nclasses=-1, ncoef=90, init_coef=0, n_heads=16):
 		super().__init__()
 
 		self.ncoef=ncoef
@@ -1040,7 +1040,9 @@ class TDNN_multipool(nn.Module):
 
 		self.stats_pooling = StatisticalPooling()
 
-		self.post_pooling_1 = nn.Sequential(nn.Linear(5*1024, 512),
+		self.multihead_pooling = nn.TransformerEncoderLayer(d_model=1024, nhead=n_heads, dim_feedforward=512, dropout=0.1)
+
+		self.post_pooling_1 = nn.Sequential(nn.Linear(1024, 512),
 			nn.ReLU(inplace=True),
 			nn.BatchNorm1d(512) )
 
@@ -1059,21 +1061,24 @@ class TDNN_multipool(nn.Module):
 		x = x.squeeze(1)
 
 		x_1 = self.model_1(x)
-		x_pool.append(self.stats_pooling(x_1))
+		x_pool.append(self.stats_pooling(x_1).unsqueeze(-1))
 
 		x_2 = self.model_2(x_1)
-		x_pool.append(self.stats_pooling(x_2))
+		x_pool.append(self.stats_pooling(x_2).unsqueeze(-1))
 
 		x_3 = self.model_3(x_2)
-		x_pool.append(self.stats_pooling(x_3))
+		x_pool.append(self.stats_pooling(x_3).unsqueeze(-1))
 
 		x_4 = self.model_4(x_3)
-		x_pool.append(self.stats_pooling(x_4))
+		x_pool.append(self.stats_pooling(x_4).unsqueeze(-1))
 
 		x_5 = self.model_5(x_4)
-		x_pool.append(self.stats_pooling(x_5))
+		x_pool.append(self.stats_pooling(x_5).unsqueeze(-1))
 
-		x_pool = torch.cat(x_pool, -1)
+		x_pool = torch.cat(x_pool, -1).permute(2,0,1)
+
+		x_pool = self.multihead_pooling(x_pool)
+		x_pool = x_pool.permute(1,2,0).mean(-1)
 
 		x = self.post_pooling_1(x_pool)
 		out = self.post_pooling_2(x)
