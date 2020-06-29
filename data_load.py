1dc06a91908d460ffa07f8b6837b36f0751b9ef7
joaomonteirof <joaomonteirof@gmail.com>
2020-06-29 11:42:43 -0400

    noise

--- data_load.py
+++ data_load.py
@@ -15,7 +15,7 @@ def augment_spec(example):
 		if random.random()>0.5:
 			example = freq_mask(example, F=50, dim=2)
 		if random.random()>0.5:
-			example += torch.randn_like(example)*random.choice([1e-1, 1e-2, 1e-3, 1e-4])
+			example += torch.randn_like(example)*random.choice([1e-1, 1e-2, 1e-3])
 
 	return example
 
